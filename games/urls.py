95561d6ea7f24526b979dbc74738827e2b321442
Phil Quinn <phillmatic19@gmail.com>
2012-04-21 13:47:31 -0700

    fixed urls again

--- games/urls.py
+++ games/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
+from games import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -20,4 +21,8 @@ urlpatterns = patterns('',
 urlpatterns += patterns('django.views.generic.simple',
     url(r'^$', 'direct_to_template', {'template': 'index.html'}),
     url(r'^events$', 'direct_to_template', {'template': 'events.html'}),
+)
+
+urlpatterns += patterns('',
+    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
