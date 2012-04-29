49de6987a2a47142d0d88a497bdce934b0835b27
Phil Quinn <phillmatic19@gmail.com>
2012-04-29 14:41:10 -0700

    initial tracker commit

--- games/settings.py
+++ games/settings.py
@@ -123,6 +123,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'tracker',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
---
+++ games/tracker/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+YEAR_CHOICES = (
+    (u'FR', u'Freshman'),
+    (u'SO', u'Sophomore'),
+    (u'MI', u'Middler'),
+    (u'JR', u'Junior'),
+    (u'SR', u'Senior')
+)
+
+UNLOCKABLE_TYPE_CHOICES = (
+    (u'ach', u'Achievement'),
+    (u'feat', u'Feat')
+)
+
+class Member(models.Model):
+    tag = models.CharField(max_length=16)
+    year = models.CharField(choices=YEAR_CHOICES)
+    nuid = models.CharField(max_length=10, primary_key=True)
+    email = models.CharField(max_length=30, unique=True)
+    achievements = models.ManyToManyField(Unlockable, through="Unlocked")
+
+class Unlockable(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    point_value = models.IntegerField()
+    type = models.CharField(max_length=4, choices=UNLOCKABLE_TYPE_CHOICES)
+
+class Unlocked(models.Model):
+    member = models.ForeignKey(Member)
+    unlockable = models.ForeignKey(Unlockable)
+    date = models.DateTimeField(auto_now_add=True)
