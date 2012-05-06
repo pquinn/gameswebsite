41c6538beb729a4442f8bc1a901b94d88ed30600
Phil Quinn <phillmatic19@gmail.com>
2012-05-06 15:50:16 -0700

    fixed logging in

--- games/settings.py
+++ games/settings.py
@@ -4,7 +4,7 @@ import os.path
 
 PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -123,7 +123,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
-    'tracker',
+    'games.tracker',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
@@ -150,3 +150,5 @@ LOGGING = {
         },
     }
 }
+
+LOGIN_REDIRECT_URL = "/"
--- games/urls.py
+++ games/urls.py
@@ -1,18 +1,12 @@
-from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
+from django.conf.urls.defaults import *
 from games import settings
+from django.contrib.auth import urls as auth_urls
+from django.contrib.auth.views import login as login_view
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'games.views.home', name='home'),
-    # url(r'^games/', include('games.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
 
@@ -22,5 +16,15 @@ urlpatterns += patterns('django.views.generic.simple',
 )
 
 urlpatterns += patterns('',
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html',
+                                                        'redirect_field_name': 'next'}),
+)
+
+urlpatterns += patterns('',
     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+)
+
+urlpatterns += patterns('',
+    # ... other URL defs and includes here ....
+    (r'', include(auth_urls)),
 )
