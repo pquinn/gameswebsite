886c602fbc3e7dc8809f3621a9e1740468db68bc
Phil Quinn <phillmatic19@gmail.com>
2012-05-20 18:28:26 -0700

    added popover dialogs to achievement view. jquery is cool.

--- games/tracker/views.py
+++ games/tracker/views.py
@@ -57,7 +57,7 @@ def leaderboard(request):
 
 def list_feats(request):
     #feats = Unlockable.objects.filter(type=u'feat')
-    unlockables = Unlockable.objects.all()
+    unlockables = Unlockable.objects.filter(is_public=False)
     param_dictionary = {"unlockables": unlockables}
     return render_to_response("tracker/unlockable-list.html",
                               param_dictionary,
