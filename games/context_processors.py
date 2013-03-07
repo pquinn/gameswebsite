1a49a58a3f891e28b0b074226dd802042bf49699
Phil Quinn <phillmatic19@gmail.com>
2013-03-06 20:02:51 -0500

    whoops

--- games/context_processors.py
+++ games/context_processors.py
@@ -4,10 +4,7 @@ from datetime import datetime
 
 def constants(context):
     # return the value you want as a dictionary. you may add multiple values in there.
-    return {'TWITTER_URL': 'http://www.twitter.com/Phillmatic19',
-            'FACEBOOK_URL': 'http://www.facebook.com/phillmatic19',
-            'SOC_URL' : 'http://careers.stackoverflow.com/pquinn',
-            }
+    return {}
 
 def time(context):
     return {'current_time': datetime.now(),}
